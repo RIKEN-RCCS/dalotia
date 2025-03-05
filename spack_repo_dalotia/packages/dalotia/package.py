01714ba6465a661a5973b1fe80d6b4bb04de550c
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-03-05 13:32:07 +0900

    spack: add initial repo + packages for dalotia and safetensors-cpp

---
+++ spack_repo_dalotia/packages/dalotia/package.py
@@ -0,0 +1,49 @@
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Dalotia(CMakePackage):
+    """Data loader for tensors in AI library """
+
+    # FIXME: Add a proper url for your package's homepage here.
+    homepage = "https://github.com/RIKEN-RCCS/dalotia"
+    git = "https://github.com/RIKEN-RCCS/dalotia"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    maintainers("freifrauvonbleifrei", "domke")
+
+    license("Apache-2.0", checked_by="freifrauvonbleifrei")
+
+    # FIXME: Add proper versions here.
+    version("main", branch="main")
+
+    variant("tests", default=True, description="build dalotia tests")
+    variant("cpp_pmr", default=True, description="use polymorphic memory resources (pmr) C++17 feature for dalotia")
+    variant("openmp", default=True, description="Build with OpenMP support")
+    variant("safetensorscpp", default=True, description="use safetensors-cpp for tensor I/O")
+    variant("fortran", default=True, description="Build Fortran interface")
+
+    depends_on("cxx", type="build")
+    depends_on("c", type="build")
+    depends_on("fortran", type="build", when="+fortran")
+    depends_on("cmake@3.24:", type="build")
+    depends_on("safetensors-cpp", when="+safetensorscpp")
+
+
+    def cmake_args(self):
+        args = [
+            self.define("DALOTIA_CPP_BUILD_EXAMPLES", True),
+            self.define_from_variant("DALOTIA_BUILD_TESTS", "tests"),
+            self.define_from_variant("DALOTIA_WITH_CPP_PMR", "cpp_pmr"),
+            self.define_from_variant("DALOTIA_WITH_OPENMP", "openmp"),
+            self.define_from_variant("DALOTIA_WITH_SAFETENSORS_CPP", "safetensorscpp"),
+            self.define_from_variant("DALOTIA_WITH_FORTRAN", "fortran"),
+        ]
+        if self.spec.satisfies("+safetensorscpp"):
+            args.append(self.define("safetensors-cpp_DIR", self.spec["safetensors-cpp"].prefix.lib.cmake))
+        return args
